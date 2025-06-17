d27517e54af3abf4166b7a6e27e5cf22cef1676a
niver.copa <vlader122@gmail.com>
2025-06-16 23:04:48 -0400

    add swagger

--- almacenes/Program.cs
+++ almacenes/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AlmacenesContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("AlmacenesConnection"));
@@ -45,6 +47,11 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My APi");
+    });
 }
 
 app.UseAuthorization();
